1d2b5438963270b61f2415264a2d61669a99898d
Kerrie <wongkerrie@gmail.com>
2021-01-20 05:01:48 +0000

    created all the functions

--- calculator.py
+++ calculator.py
@@ -5,10 +5,38 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
-user_input = input(">")
-tokens = user_input.split(" ")
+user_input = input("Enter your equation>")
+token= user_input.split(" ")
+operator = token[0]
 
-if tokens[0]== 'pow':
-    answer = float(tokens[1]) ** float(tokens[2])
-    print(answer)
-print(tokens)
+if operator == 'q':
+    quit
+else:
+    num1 = token[1]
+
+    if operator == 'square':
+        result = square(float(num1))
+
+    elif operator == 'cube':
+        result = cube(float(num1))
+
+    else:
+        num2 = token[2]
+        if operator == '+':
+            result = add(float(num1),float(num2)) 
+
+        elif operator== '-':
+            result = subtract(float(num1),float(num2))
+
+        elif operator == '*':
+            result = multiply(float(num1), float(num2))
+            
+        elif operator == '/':
+            result = divide(float(num1), float(num2))
+
+        elif operator == 'pow':
+            result = pow(float(num1), float(num2))
+
+        elif operator == 'mod':
+            result = mod(float(num1), float(num2))
+    print(result)
